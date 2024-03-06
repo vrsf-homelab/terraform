d1b72b7efc37f7740030d7940bb1906bcc354659
Paweł Farys <pawel.farys@ringieraxelspringer.pl>
2024-03-06 22:20:47 +0100

    Some of cleanups :)

--- aws/core/modules.tf
+++ aws/core/modules.tf
@@ -1,16 +1,3 @@
 module "common" {
   source = "../modules/common"
 }
-
-module "oidc_github" {
-  source  = "vrs-factory/oidc-github/aws"
-  version = "1.0.0"
-
-  url        = "https://token.actions.githubusercontent.com"
-  client_ids = ["sts.amazonaws.com"]
-  extra_thumbprints = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
-  ]
-
-  tags = module.common.tags
-}
