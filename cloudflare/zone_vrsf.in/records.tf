01c5558495c4a83cdd5ee7e559d4d8b5d3806f95
Paweł Farys <pmg.farys@gmail.com>
2024-05-14 21:43:55 +0200

    Added a new node for Proxmox

--- cloudflare/zone_vrsf.in/records.tf
+++ cloudflare/zone_vrsf.in/records.tf
@@ -27,10 +27,19 @@ resource "cloudflare_record" "adguard_panel" {
 ###
 ##  P R O X M O X
 ###
-resource "cloudflare_record" "proxmox_panel" {
+resource "cloudflare_record" "proxmox_crimson" {
   zone_id = cloudflare_zone.default.id
   name    = "crimson.pve"
   value   = "10.69.30.10"
+  type    = "A"
+  proxied = false
+  comment = local.comment
+}
+
+resource "cloudflare_record" "proxmox_emerald" {
+  zone_id = cloudflare_zone.default.id
+  name    = "emerald.pve"
+  value   = "10.69.30.11"
   type    = "A"
   proxied = false
   comment = local.comment
@@ -49,10 +58,10 @@ resource "cloudflare_record" "k8s_cluster" {
   comment = local.comment
 }
 
-resource "cloudflare_record" "k8s_cluster_workers" {
+resource "cloudflare_record" "k8s_cluster_ingress" {
   zone_id = cloudflare_zone.default.id
   name    = "*"
-  value   = "10.69.35.11"
+  value   = "10.69.36.1"
   type    = "A"
   proxied = false
   comment = local.comment
