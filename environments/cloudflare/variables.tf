eb47b9ec9c74800a18f0baacb6daeebe0427675e
Paweł Farys <pmg.farys@gmail.com>
2024-08-06 22:34:22 +0200

    Removed no longer used AWS variables

--- environments/cloudflare/variables.tf
+++ environments/cloudflare/variables.tf
@@ -1,15 +1,3 @@
-## AWS credentials required for backend
-variable "aws_access_key" {
-  type = string
-}
-variable "aws_secret_key" {
-  type = string
-}
-variable "aws_region" {
-  type    = string
-  default = "eu-central-1"
-}
-
 variable "api_token" {
   type = string
 }
--- environments/vault/variables.tf
+++ environments/vault/variables.tf
@@ -1,12 +1,3 @@
-#### AWS State Backend ####
-variable "aws_access_key" {
-  type = string
-}
-
-variable "aws_secret_key" {
-  type = string
-}
-
 #### Vault ####
 variable "vault_address" {
   type = string
